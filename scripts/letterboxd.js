dc66d67714ab3c14734c0e0f0b65a39f06971de1
George Nakhle <1229gn@gmail.com>
2023-01-25 21:22:01 -0800

    Changed to corsproxy.io

--- scripts/letterboxd.js
+++ scripts/letterboxd.js
@@ -37,64 +37,56 @@ function isRewatch (bool) {
     return "";
 }
 
-const RSS_URL = "https://cors-anywhere.herokuapp.com/https://letterboxd.com/bluepinata/rss/";
+const RSS_URL = 'https://corsproxy.io/?' + encodeURIComponent('https://letterboxd.com/bluepinata/rss/');;
 const zeroPad = (num, places) => String(num).padStart(places, '0');
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 fetch(RSS_URL)
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => {
-    document.getElementById('filmDiary').innerHTML = ""
+    .then(response => response.text())
+    .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+    .then(data => {
+        items = data.getElementsByTagName("channel")[0].getElementsByTagName("item");
 
-    items = data.getElementsByTagName("channel")[0].getElementsByTagName("item");
+        if (items != null || items[i].getElementsByTagName("link")[0].innerHTML == null)
+            document.getElementById('filmDiary').innerHTML = "";
 
-    var html = ``;
-    var posterWidth = 70;
-    var posterHeight = 105;
-    var link = ".";
-    var watchedDate;
-    var posterLink;
-    var filmTitle;
-    var watchedDate;
-    var rating = 0; 
-    var today = new Date();
+        var html = ``;
+        var posterWidth = 70;
+        var posterHeight = 105;
+        var link = ".";
+        var watchedDate;
+        var posterLink;
+        var filmTitle;
+        var watchedDate;
+        var rating = 0; 
+        var today = new Date();
 
-    for (var i = 0; i < 5; i++) {
-        link = getLinkToLetterboxd(items[i].getElementsByTagName("link")[0].innerHTML);
-        posterLink = getLinkFromDescription(items[i].getElementsByTagName("description")[0].innerHTML);
-        filmTitle = items[i].getElementsByTagName("letterboxd:filmTitle")[0].innerHTML.replace('amp;', '');
-        filmYear = items[i].getElementsByTagName("letterboxd:filmYear")[0].innerHTML;
-        watchedDate = new Date(items[i].getElementsByTagName("letterboxd:watchedDate")[0].innerHTML);
-        watchedDate = `${months[watchedDate.getMonth()]} ${addDayPostfix(watchedDate.getDate())}, ${watchedDate.getFullYear()}`;
-        if (items[i].getElementsByTagName("letterboxd:memberRating")[0] != undefined)
-            rating = numberToStars(items[i].getElementsByTagName("letterboxd:memberRating")[0].innerHTML);
-        else
-            rating = numberToStars(0);
-        rewatch = isRewatch(items[i].getElementsByTagName("letterboxd:rewatch")[0].innerHTML);
+        for (var i = 0; i < 5; i++) {
+            link = getLinkToLetterboxd(items[i].getElementsByTagName("link")[0].innerHTML);
+            posterLink = getLinkFromDescription(items[i].getElementsByTagName("description")[0].innerHTML);
+            filmTitle = items[i].getElementsByTagName("letterboxd:filmTitle")[0].innerHTML.replace('amp;', '');
+            filmYear = items[i].getElementsByTagName("letterboxd:filmYear")[0].innerHTML;
+            watchedDate = new Date(items[i].getElementsByTagName("letterboxd:watchedDate")[0].innerHTML);
+            watchedDate = `${months[watchedDate.getMonth()]} ${addDayPostfix(watchedDate.getDate())}, ${watchedDate.getFullYear()}`;
+            if (items[i].getElementsByTagName("letterboxd:memberRating")[0] != undefined)
+                rating = numberToStars(items[i].getElementsByTagName("letterboxd:memberRating")[0].innerHTML);
+            else
+                rating = numberToStars(0);
+            rewatch = isRewatch(items[i].getElementsByTagName("letterboxd:rewatch")[0].innerHTML);
 
-        // Couldn't get anything from RSS
-        if (i == 0 && items == null) {
-            link = ".";
-            posterLink = "../images/socials/letterboxd.jpg";
-            filmTitle = "Untitled"
-            filmYear = today.getFullYear();
-            watchedDate = `${months[today.getMonth()]} ${addDayPostfix(today.getDate())}, ${today.getFullYear()}`;
-            rating = numberToStars("5.0");
+            html = `
+                <a href="${link}" target="_blank">
+                    <li>
+                        <img class="filmPoster" src="${posterLink}" width="${posterWidth}" height="${posterHeight}" alt="${filmTitle} (${filmYear})">
+                        <ul class="filmDiaryEntry">
+                            <li class="filmHeader"><span class="filmTitle">${filmTitle} <span class="filmYear">(${filmYear})</span></li>
+                            <li class="filmSubtitle"><span class="watchedDate">${watchedDate}</span> <span class="filmRating">${rating}${rewatch}</span></li>
+                        </ul>
+                    </li>
+                </a>
+                <hr class="filmHR">`;
+            
+            document.getElementById('filmDiary').innerHTML += html;
         }
-
-        html = `
-            <a href="${link}" target="_blank">
-                <li>
-                    <img class="filmPoster" src="${posterLink}" width="${posterWidth}" height="${posterHeight}" alt="${filmTitle} (${filmYear})">
-                    <ul class="filmDiaryEntry">
-                        <li class="filmHeader"><span class="filmTitle">${filmTitle} <span class="filmYear">(${filmYear})</span></li>
-                        <li class="filmSubtitle"><span class="watchedDate">${watchedDate}</span> <span class="filmRating">${rating}${rewatch}</span></li>
-                    </ul>
-                </li>
-            </a>
-            <hr class="filmHR">`;
-        
-        document.getElementById('filmDiary').innerHTML += html;
     }
-  });
+  );
