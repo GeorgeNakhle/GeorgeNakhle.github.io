f3e28009f9f47f3c8acf711ac57565a7afe47422
George Nakhle <1229gn@gmail.com>
2023-02-07 02:16:10 -0800

    asideActivity.js bug on socials

---
+++ scripts/asideActivity.js
@@ -0,0 +1,24 @@
+const sections = document.querySelectorAll("section");
+var links = document.querySelectorAll(".sidebarListItem");
+links = document.querySelectorAll("a");
+
+function asideActivity() {
+    var scrollPosition = window.scrollY + 80;
+
+    sections.forEach(section => {
+        if (scrollPosition >= section.offsetTop) {
+            links.forEach(link => {
+                link.classList.remove("activeSidebarListItem");
+                if (section.getAttribute("id") === link.getAttribute("href").substring(1)) {
+                    link.classList.add("activeSidebarListItem");
+                }
+            });
+        }
+    });
+}
+
+["load", "scroll"].forEach(e => 
+    window.addEventListener(e, asideActivity)
+);
+
+// Source: https://permaslug.com/add-a-class-based-on-scroll-position-with-javascript/
